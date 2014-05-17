b0547cb8601ad59340319a01cd6b9fac925d5b54
bagustris <bagustris@yahoo.com>
2014-05-18 04:18:05 +0900

    initial commit

---
+++ prog33.c
@@ -0,0 +1,14 @@
+/*
+ * prog33.c
+ * displaying multiple lines of output
+ * 05/18/2014 04:11:21 AM
+ * bagustris (bagustris@mail.com)
+ */
+
+#include <stdio.h>
+int main (void)
+{
+    printf ("Testing...\nTesting...\nTesting...\n");
+
+    return 0;    
+}
