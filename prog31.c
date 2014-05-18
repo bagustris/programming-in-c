a75d1ca4317e46a4c260a4aede8636910839edee
bagustris <bagustris@linuxmail.org>
2014-05-18 12:48:55 +0900

    initial 2nd

---
+++ prog31.c
@@ -0,0 +1,14 @@
+/* prog31.c
+ * c is fun
+ * 05/09/2014 
+ * bagustris, bagustris@yahoo.com
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+  printf ("Programming is fun.\n");
+
+  return 0;
+}
---
+++ prog32.c
@@ -0,0 +1,16 @@
+/* prog33.c
+ * demoed printf routine
+ * 05/08/2014 
+ * bagustris, bagustris@yahoo.com
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+  printf ("Programming is fun.\n");
+  printf ("And programming in C is even more fun.\n");
+
+  return 0;
+
+}
